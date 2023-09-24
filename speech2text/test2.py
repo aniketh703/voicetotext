1947d42224ff3a1a1d6c4e68d2ee860919cb1dcc
Aniketh Vustepalle <anikethvustepalle03@gmail.com>
2023-09-24 18:23:31 +0530

    modified:   speech2text/test2.py

--- speech2text/test2.py
+++ speech2text/test2.py
@@ -1,6 +1,6 @@
 import speech_recognition as sr
-from translate import Translator
 from langdetect import detect
+from translate import Translator
 
 r = sr.Recognizer()
 
@@ -8,11 +8,19 @@ mic = sr.Microphone()
 
 transcriptions = {}
 
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    print("Say something!")
-    while True:
-        audio = r.listen(source)
+recording = False
+
+while True:
+    if not recording:
+        input("Press Enter to start recording...")
+        recording = True
+        with mic as source:
+            r.adjust_for_ambient_noise(source)
+            print("Recording started. Say something!")
+            audio = r.listen(source)
+    else:
+        input("Press Enter to stop recording...")
+        recording = False
         try:
             text = r.recognize_google(audio)
             detected_language = detect(text) 
@@ -21,9 +29,12 @@ with mic as source:
             print(f'Spoken text ({detected_language}): {text}')
             target_language = input("Select a language code for translation (e.g., en for English): ")
             if target_language:
-                translator = Translator(to_lang=target_language)
-                translation = translator.translate(text)
-                print(f'Translation to {target_language}: {translation}')
+                try:
+                    translator = Translator(to_lang=target_language)
+                    translation = translator.translate(text)
+                    print(f'Translation to {target_language}: {translation}')
+                except ValueError:
+                    print(f"Invalid language code: {target_language}")
             else:
                 print("No target language selected. Skipping translation.")
 
